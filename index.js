2adacc0926dc6e85f0fef7ff6092e92d63df0a2c
weedskimhj <kimhj@weedscomm.com>
2021-04-22 15:16:51 +0900

    static folder load

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express()
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 // })
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
